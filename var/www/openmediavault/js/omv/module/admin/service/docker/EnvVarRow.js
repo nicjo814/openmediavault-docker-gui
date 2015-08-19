f83bdb28483e98912d53713745f6833e0f94a3ea
Niclas Berglind <nb@kjam.se>
2015-08-19 22:35:05 +0200

    Better validation of EnvVars component

--- var/www/openmediavault/js/omv/module/admin/service/docker/EnvVarRow.js
+++ var/www/openmediavault/js/omv/module/admin/service/docker/EnvVarRow.js
@@ -25,7 +25,9 @@ Ext.define("OMV.module.admin.service.docker.EnvVarRow", {
 			id: "envName-" + me.envCount,
 			value: me.nameVal,
 			flex: 1,
-			readOnly: defVal
+			readOnly: defVal,
+			regex: /^[a-zA-Z_]+[a-zA-Z0-9_]*$/,
+			invalidText: "Invalid name of environment value"
 		},{
 			xtype: "textfield",
 			name: "envValue-" + me.envCount,
@@ -44,8 +46,7 @@ Ext.define("OMV.module.admin.service.docker.EnvVarRow", {
 			listeners: {
 				scope: this,
 				click: function(button, e , eOpts) {
-					var errorMsg = me.validateData();
-					if(errorMsg === "") {
+					if(me.queryById("envName-" + me.envCount).isValid()) {
 						me.up('window').envVars[me.envCount] = {
 							name: me.queryById("envName-" + me.envCount).getValue(),
 							value: me.queryById("envValue-" + me.envCount).getValue()
@@ -61,7 +62,7 @@ Ext.define("OMV.module.admin.service.docker.EnvVarRow", {
 						me.queryById("envName-" + me.envCount).setReadOnly(true);
 						me.queryById("envValue-" + me.envCount).setReadOnly(true);
 					} else {
-						Ext.Msg.alert("Bad input", errorMsg);
+						Ext.Msg.alert("Error", "Bad input detected");
 					}
 				},
 				setNewRow: function() {
@@ -105,15 +106,5 @@ Ext.define("OMV.module.admin.service.docker.EnvVarRow", {
 		Ext.apply(me, {
 		});
 		me.callParent(arguments);
-	},
-
-	validateData: function() {
-		var me = this;
-		var name = me.queryById("envName-" + me.envCount).getValue();
-		var errorMsg = "";
-		if (!(/^[a-zA-Z_]+[a-zA-Z0-9_]*$/.test(name))) {
-			errorMsg = errorMsg + "Invalid name supplied";
-		}
-		return errorMsg;
 	}
 });
