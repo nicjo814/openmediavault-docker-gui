137d21e53a9048afd990f87ff3b309069719f24e
Niclas Berglind <nb@kjam.se>
2015-08-22 11:41:00 +0200

    Add GUI controls to copy Docker images/containers

--- var/www/openmediavault/js/omv/module/admin/service/docker/Settings.js
+++ var/www/openmediavault/js/omv/module/admin/service/docker/Settings.js
@@ -3,6 +3,7 @@
 // require("js/omv/module/admin/service/docker/ImageGrid.js")
 // require("js/omv/workspace/window/plugin/ConfigObject.js")
 // require("js/omv/form/field/SharedFolderComboBox.js")
+// require("js/omv/module/admin/service/docker/RootFolderBrowser.js")
 
 Ext.define("OMV.module.admin.service.docker.Settings", {
     extend: "OMV.workspace.form.Panel",
@@ -30,6 +31,13 @@ Ext.define("OMV.module.admin.service.docker.Settings", {
             var settingsPanel = parent.down("panel[title=" + _("Settings") + "]");
             var dockerVersion = settingsPanel.findField("version").getValue();
 			var checked = settingsPanel.findField("enabled").checked
+			settingsPanel.findField("destpath").setValue(settingsPanel.findField("sharedfolderref").getValue());
+			var copyDisabled = true;
+			if(!(settingsPanel.findField("orgpath").getValue() === "") && !(settingsPanel.findField("destpath").getValue() === "")) {
+				copyDisabled = false;
+			}
+			settingsPanel.queryById("dockerCopyButton").setDisabled(copyDisabled);
+
 
 			if (overviewPanel) {
 				if (checked) {
@@ -86,7 +94,89 @@ Ext.define("OMV.module.admin.service.docker.Settings", {
 					text: _("The location of the Docker base path (this setting is optional and defaults to /var/lib/docker if unset). The plugin must be enabled for a change to be committed")
 				}],
 				allowNone: true,
-				allowBlank: true
+				allowBlank: true,
+				listeners: {
+					scope: this,
+					change: function(combo, newValue, oldValue, eOpts) {
+						this.getForm().findField("destpath").setValue(newValue);
+					}
+				}
+			},{
+				xtype: "container",
+				layout: "hbox",
+				padding: "0 0 10 0",
+				items: [{
+					xtype: "textfield",
+					name: "orgpath",
+					flex: 1,
+					triggers: {
+						folder : {
+							cls     : Ext.baseCSSPrefix + "form-folder-trigger",
+							handler : "onTriggerClick"
+						}
+					},
+					onTriggerClick : function() {
+						Ext.create("OMV.window.RootFolderBrowser", {
+							listeners : {
+								scope  : this,
+								select : function(wnd, node, path) {
+									// Set the selected path.
+									this.setValue(path);
+								}
+							}
+						}).show();
+					},
+					plugins: [{
+						ptype: "fieldinfo",
+						text: _("Source path for copying of Docker image/container data")
+					}],
+					padding: "0 10 0 0",
+					listeners: {
+						scope: this,
+						change: function(field, newValue, oldValue, eOpts) {
+							if((newValue !== "") && (this.getForm().findField("destpath").getValue() !== "")) {
+								this.queryById("dockerCopyButton").setDisabled(false);
+							} else {
+								this.queryById("dockerCopyButton").setDisabled(true);
+							}
+						}
+					}
+				},{
+					xtype: "sharedfoldercombo",
+					name: "destpath",
+					plugins: [{
+						ptype: "fieldinfo",
+						text: _("Destination path")
+					}],
+					allowNone: true,
+					allowBlank: true,
+					readOnly: true,
+					flex: 1,
+					listeners: {
+						scope: this,
+						change: function(combo, newValue, oldValue, eOpts) {
+							if((newValue !== "") && (this.getForm().findField("orgpath").getValue() !== "")) {
+								this.queryById("dockerCopyButton").setDisabled(false);
+							} else {
+								this.queryById("dockerCopyButton").setDisabled(true);
+							}
+						}
+					}
+				},{
+					xtype: "button",
+					itemId: "dockerCopyButton",
+					icon: "images/docker_copy.png",
+					iconCls: Ext.baseCSSPrefix + "btn-icon-16x16",
+					flex: 0,
+					width: 24,
+					tooltip: "Start copying of data",
+					listeners: {
+						scope: this,
+						click: function(button, e , eOpts) {
+
+						}
+					}
+				}]
 			}]
 		},{
 			xtype: "fieldset",
